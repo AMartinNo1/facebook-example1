4b1c28b5818e22bce244f44d985d05ba09565ce8
Alexander Nostadt <info.globetrotter.alex@gmail.com>
2020-04-16 20:36:39 +0000

    first basic facebook example

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 # Facebook API + Gitpod.io example
 
 Example project to play with Facebook API and gitpod.io.
+
+## How To:
+
+1. Create config file (`cp config.php.example config.php`)
+2. Create a new app (https://developers.facebook.com/apps/)
+3. Fetch App ID and App Secret
+![App ID](DocAssets/facebook-app-id.png)
+
+![App Secret](DocAssets/facebook-app-id-and-app-secret.png)
+
+3. Generate Access Token (https://developers.facebook.com/tools/explorer/)
+
+4. 
--- index.php
+++ index.php
@@ -1,2 +1,48 @@
 <?php
-echo "Hello World";
+include 'vendor/autoload.php';
+include 'config.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>
+    My Name 
+  </title>
+</head>
+
+<body>
+  <h1>Get My Name from Facebook</h1>
+<?php
+
+$fb = new \Facebook\Facebook([
+  'app_id' => FB_APP_ID,           //Replace {your-app-id} with your app ID
+  'app_secret' => FB_APP_SECRET,   //Replace {your-app-secret} with your app secret
+  'graph_api_version' => 'v5.0',
+]);
+
+try {
+   
+// Get your UserNode object, replace {access-token} with your token
+  $response = $fb->get('/me', FB_ACCESS_TOKEN);
+
+} catch(\Facebook\Exceptions\FacebookResponseException $e) {
+        // Returns Graph API errors when they occur
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(\Facebook\Exceptions\FacebookSDKException $e) {
+        // Returns SDK errors when validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$me = $response->getGraphUser();
+
+       //All that is returned in the response
+echo 'All the data returned from the Facebook server: ' . $me;
+
+       //Print out my name
+echo 'My name is ' . $me->getName();
+
+?>
+</body>
+</html>
